34513edbc6ba8e977debe0ebd79216b63ceda42a
af1988 <aikorf2002@gmail.com>
2024-10-08 11:12:19 -0400

    hi

--- src/models/CaculatorObersever.ts
+++ src/models/CaculatorObersever.ts
@@ -2,6 +2,6 @@ import { ICalculatorObserver } from "../interfaces/ICaculatorObserver";
 
 export class CaculatorObserver implements ICalculatorObserver{
     update(message: string): void {
-        throw new Error("Method not implemented.");
+        print: message;
     }
 }
